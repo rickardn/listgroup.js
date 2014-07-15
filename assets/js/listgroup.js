b99f452630624420ea042bcc71a7df2ed353d59b
Rickard Nilsson <rickard@rickardnilsson.net>
2014-07-15 21:41:39 +0200

    Initial refactoring into objects

--- assets/js/listgroup.js
+++ assets/js/listgroup.js
@@ -12,12 +12,68 @@
         unselect,
         add;
 
+    // split up in two classes?
+    // one for handling list-groups
+    // and one for handling select elements
+    // first: split up into one class
+
+    var SelectList = function(element, options) {
+        this.$element = $(element);
+        this.options = options;
+        this.createListGroup();
+    };
+
+    SelectList.prototype.createListGroup = function() {
+        var $group = this.$element;
+
+        // create a list-group
+        var listGroup = $('<ul class="list-group"></ul>');
+
+        if ($group.attr('multiple'))
+            listGroup.data('toggle', 'buttons');
+
+        $group.find('option').each(function (j, item) {
+            var html = '<a href="#" class="list-group-item" ';
+            html += 'data-value="' + $(item).val() + '">'
+                 + $(item).text() + '</a>';
+
+            var $item = $(html);
+            listGroup.append($item);
+        });
+
+        $group.change(function (e) {
+            listGroup.listgroup({ select: $group.val() });
+        });
+
+        listGroup.listgroup({
+            select: $group.val(),
+            click: function () {
+                $group.val($(this).data('value'));
+            }
+        });
+        $group.before(listGroup);
+    };
+
+    
+    var ListGroup = function(element, options) {
+        this.$element = $(element);
+        this.options = options;
+    };
+
+    ListGroup.prototype.select = function(item) {
+        select(this.$element, item);
+    };
+
+    ListGroup.prototype.unselect = function(item) {
+        unselect(this.$element, item);
+    };
+
     select = function (group, item) {
-        if ($(group).data('toggle') != 'buttons')
+        //todo: if ($(group).data('toggle') != 'buttons')
             unselect(group);
 
         if (item instanceof $)
-            $(item)
+            item
                 .addClass('active')
                 .blur();
         
@@ -53,9 +109,12 @@
             config = option;
 
         init = function (group) {
+            // TODO: move to constructor
             if (config && config.toggle)
                 $(group).data('toggle', config.toggle);
 
+            // TODO: move to constructor?
+            // TODO: refactor to .on('click'
             $('.list-group-item', group).click(function() {
                 var $item = $(this);
 
@@ -68,42 +127,27 @@
             });
         };
 
-        this.each(function(i, group) {
+        this.each(function (i, group) {
+
+            // TODO: refactor to select class
             if ($(group).prop('tagName') === 'SELECT') {
-                var $group = $(group);
-                // create a list-group
-                var listGroup = $('<ul class="list-group"></ul>');
 
-                if ($group.attr('multiple'))
-                    listGroup.data('toggle', 'buttons');
+                var sl = new SelectList(group, option);
 
-                $group.find('option').each(function(j, item) {
-                    var html = '<a href="#" class="list-group-item" ';
-                    html += 'data-value="' + $(item).val() + '">'
-                         + $(item).text() + '</a>';
-
-                    var $item = $(html);
-                    listGroup.append($item);
-                });
-
-                $group.change(function(e) {
-                    listGroup.listgroup({ select: $group.val() });
-                });
-                
-                listGroup.listgroup({
-                    select: $group.val(),
-                    click: function () {
-                        $group.val($(this).data('value'));
-                    }
-                });
-                $group.before(listGroup);
-                
+                return true;
             }
+
+            // else group is a list-group
+
+            var data = $(group).data('listgroup');
+            if (!data) $(group).data('listgroup', (data = new ListGroup(group, option)));
+
 
             if (typeof config === 'object') {
 
                 if (config.unselect) {
-                    unselect(group, config.unselect);
+                    //unselect(group, config.unselect);
+                    data.unselect(config.unselect);
                 }
 
                 if (config.add) {
@@ -111,10 +155,13 @@
                 }
 
                 if (config.select) {
-                    select(group, config.select);
+                    //select(group, config.select);
+                    data.select(config.select);
                 }
 
+                // note: should it be possible to redefine click?
                 if (config.click) {
+                    // move to constructor?
                     init(group);
                 }
 
