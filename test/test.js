332e0f40f0af03657d18380eefebaa6e9a2137b0
Rickard Nilsson <rickard@rickardnilsson.net>
2014-07-28 21:09:23 +0200

    More qunit tests for 100% code coverage

--- test/test.js
+++ test/test.js
@@ -16,35 +16,6 @@
         strictEqual($listgrp[0], $el[0], 'collection contains element')
     })
 
-    test('should toggle active', function() {
-        var $listgrp = $('<ul class="list-group"/>').listgroup()
-        var $item = $('<li class="list-group-item">Foo</li>')
-        $listgrp
-            .append($item)
-            .appendTo('#qunit-fixture')
-        ok(!$item.hasClass('active'), 'item does not have active class')
-        $item.click()
-        ok($item.hasClass('active'), 'item has class active')
-    })
-
-    test('should switch active in single selection list', function() {
-        var $listgrp = $('<ul class="list-group"/>').listgroup()
-        var $item1 = $('<li class="list-group-item">Foo</li>')
-        var $item2 = $('<li class="list-group-item">Boo</li>')
-        $listgrp
-            .append($item1)
-            .append($item2)
-            .appendTo('#qunit-fixture')
-        ok(!$item1.hasClass('active'), 'item1 does not have active class')
-        ok(!$item2.hasClass('active'), 'item2 does not have active class')
-        $item1.click()
-        ok($item1.hasClass('active'), 'item1 has class active')
-        ok(!$item2.hasClass('active'), 'item2 does not have active class')
-        $item2.click()
-        ok(!$item1.hasClass('active'), 'item1 does no longer have active class')
-        ok($item2.hasClass('active'), 'item2 now have active class')
-
-    })
     test('should switch active in single select anchor list group', function() {
         expect(6)
         clickOnEachItem('#anchor-listgroup1')
@@ -54,12 +25,6 @@
         expect(6)
         clickOnEachItem('#ul-listgroup1')
     })
-
-    function single(collection) {
-        if (collection.length != 1)
-            ok(false, 'expected exactly 1 element, actually ' + collection.length)
-        return collection[0]
-    }
 
     function clickOnEachItem(selection) {
         var items = $(selection).listgroup().children()
@@ -72,6 +37,27 @@
             deepEqual(activeItem, items[i], 'click on item ' + (i + 1) + ', only this is active')
         }
     }
+
+    function single(collection) {
+        if (collection.length != 1)
+            ok(false, 'expected exactly 1 element, actually ' + collection.length)
+        return collection[0]
+    }
+
+    module('list group multi select')
+
+    test('should toggle active in multi select list group', function() {
+        var $listgroup = $('<ul class="list-group" data-toggle="items">')
+                            .append('<li class="list-group-item">foo</li>')
+                            .listgroup(),
+            $item = $listgroup.find('.list-group-item');
+
+        equal($listgroup.find('.active').length, 0, 'item is not active')
+        $item.click()
+        equal($listgroup.find('.active').length, 1, 'item is active')
+        $item.click()
+        equal($listgroup.find('.active').length, 0, 'item is not active')
+    })
 
     module('select element')
 
@@ -88,7 +74,25 @@
             var $option = $($select.children().get(i))
             equalItems($(item), $option)
         });
+    })
 
+    test('should update select when list group is clicked', function() {
+        var $select = $('<select multiple><option>foo</option></select>').listgroup(),
+            $item = $select.siblings('.list-group').find('.list-group-item')
+
+        equal($select.val(), null, 'default value of multiple select is null')
+        $item.click()
+        equal($select.val(), 'foo', 'select is updated when list group was clicked')
+    })
+
+    test('should update list group when select elements change event triggers', function() {
+        var $select = $('<select multiple><option>foo</option></select>').listgroup(),
+            $item = $select.siblings('.list-group').find('.list-group-item')
+
+        ok(!$item.hasClass('active'), 'no items are selected by default')
+        $select.val('foo')
+        $select.change()
+        ok($item.hasClass('active'), 'list group is updated when select element was changed')
     })
 
     test('should generate multi select list group', function () {
@@ -117,5 +121,108 @@
         $listGroup.find('.list-group-item').each(function(i, item) {
             ok($(item).hasClass('hidden'), 'list group item is hidden')
         })
+    })
+
+    module('javascript api')
+
+    test('should set multiple select with toggle option', function() {
+        var $listgroup = $('<ul class="list-group"><li class="list-group-item">foo</li></ul>')
+            .listgroup({toggle: 'items'})
+
+        equal($listgroup.data('toggle'), 'items', 'multiple select is enabled')
+    })
+
+    module('javascript api select element')
+
+    test('should select single item by value', function () {
+        expect(1)
+
+        var $select = $('<select multiple>_</select>')
+                        .append('<option value="1">foo</option>')
+                        .append('<option value="2">foo</option>')
+                        .listgroup({ select: "1" })
+
+        var $listgroup = $select.siblings('.list-group')
+
+        $listgroup.find('.list-group-item.active').each(function (i, item) {
+            ok($(item).hasClass('active'), 'item ' + $(item).data('value') + ' is selected by selection')
+        })
+    })
+
+    test('should select multiple items by value', function () {
+        expect(2)
+
+        var $select = $('<select multiple>_</select>')
+                        .append('<option value="1">foo</option>')
+                        .append('<option value="2">foo</option>')
+                        .listgroup({ select: ["1", "2"] })
+
+        var $listgroup = $select.siblings('.list-group')
+
+        $listgroup.find('.list-group-item.active').each(function (i, item) {
+            ok($(item).hasClass('active'), 'item ' + $(item).data('value') + ' is selected by selection')
+        })
+    })
+
+    test('should select single item by selection', function () {
+        expect(1)
+
+        var $select = $('<select multiple>_</select>')
+                        .append('<option value="1">foo</option>')
+                        .append('<option value="2">foo</option>')
+
+        $select.listgroup({ select: $select.find('option:last') })
+
+        var $listgroup = $select.siblings('.list-group')
+
+        $listgroup.find('.list-group-item.active').each(function (i, item) {
+            ok($(item).hasClass('active'), 'item ' + $(item).data('value') + ' is selected by selection')
+        })
+    })
+
+    test('should select multiple items by selection', function () {
+        expect(2)
+
+        var $select = $('<select multiple>_</select>')
+                        .append('<option value="1">foo</option>')
+                        .append('<option value="2">foo</option>')
+
+        $select.listgroup({ select: $select.find('option') })
+
+        var $listgroup = $select.siblings('.list-group')
+
+        $listgroup.find('.list-group-item').each(function (i, item) {
+            ok($(item).hasClass('active'), 'item ' + $(item).data('value') + ' is selected by selection')
+        })
+    })
+
+    test('should unselect single item by value', function () {
+        var $select = $('<select multiple>_</select>')
+                        .append('<option value="1" selected>foo</option>')
+                        .append('<option value="2">foo</option>')
+                        .listgroup()
+                        
+        var $listgroup = $select.siblings('.list-group')
+
+        equal($listgroup.find('.list-group-item.active').length, 1, 'first item is selected')
+
+        $select.listgroup({ unselect: "1" })
+
+        equal($listgroup.find('.list-group-item.active').length, 0, 'first item is unselected')
+    })
+
+    test('should unselect multiple items by value', function () {
+        var $select = $('<select multiple>_</select>')
+                        .append('<option value="1" selected>foo</option>')
+                        .append('<option value="2" selected>foo</option>')
+                        .listgroup()
+
+        var $listgroup = $select.siblings('.list-group')
+
+        equal($listgroup.find('.list-group-item.active').length, 2, 'all items are selected')
+
+        $select.listgroup({ unselect: ["1", "2"] })
+
+        equal($listgroup.find('.list-group-item.active').length, 0, 'all item are unselected')
     })
 })
